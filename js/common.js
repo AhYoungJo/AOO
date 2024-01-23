df24de7ee5d65e1023686a3a08cc5cd47975ad16
kss2408 <na123kr@gmail.com>
2024-01-23 16:10:12 +0900

    호버 기능 미디어쿼리 적용 모바일시 비활성화

--- js/common.js
+++ js/common.js
@@ -1,68 +1,135 @@
 $(document).ready(function () {
-    $(".introMenu a").mouseover(function () {
-        $(".introSubMenu ul li").css({
-            visibility: "visible",
-            opacity: "1",
-            transition: "1s",
-            top: "2vh",
-        });
-    });
-});
-$(document).ready(function () {
-    $(".activityMenu a").mouseover(function () {
-        $(".activitySubMenu ul li").css({
-            visibility: "visible",
-            opacity: "1",
-            transition: "1s",
-            top: "2vh",
-        });
-    });
-});
-$(document).ready(function () {
-    $(".businessMenu a").mouseover(function () {
-        $(".businessSubMenu ul li").css({
-            visibility: "visible",
-            opacity: "1",
-            transition: "1s",
-            top: "2vh",
-        });
-    });
-});
+    // 미디어 쿼리 객체 생성
+    const mediaQuery = window.matchMedia('(min-width: 601px)');
 
-$(document).ready(function () {
-    $(".communicationMenu a").mouseover(function () {
-        $(".communicationSubMenu ul li").css({
-            visibility: "visible",
-            opacity: "1",
-            transition: "1s",
-            top: "2vh",
-        });
-    });
-});
+    // 화면 크기에 따라 이벤트 활성화 또는 비활성화
+    function handleMediaQuery() {
+        if (mediaQuery.matches) {
+            // 화면 크기가 601px 이상인 경우
 
-$(document).ready(function () {
-    $(".donationMenu a").mouseover(function () {
-        $(".donationSubMenu ul li").css({
-            visibility: "visible",
-            opacity: "1",
-            transition: "1s",
-            top: "2vh",
-        });
-    });
-});
+            $('.introMenu a').mouseover(function () {
+                $('.introSubMenu ul li').css({
+                    visibility: 'visible',
+                    opacity: '1',
+                    transition: '1s',
+                    top: '2vh',
+                });
+            });
 
-window.addEventListener("mouseout", function () {
-    $(".main-menu ul li").css({
-        visibility: "hidden",
-        opacity: "0",
-        transition: "0.5s",
-        top: "0px",
+            $('.activityMenu a').mouseover(function () {
+                $('.activitySubMenu ul li').css({
+                    visibility: 'visible',
+                    opacity: '1',
+                    transition: '1s',
+                    top: '2vh',
+                });
+            });
+
+            $('.businessMenu a').mouseover(function () {
+                $('.businessSubMenu ul li').css({
+                    visibility: 'visible',
+                    opacity: '1',
+                    transition: '1s',
+                    top: '2vh',
+                });
+            });
+            $('.communicationMenu a').mouseover(function () {
+                $('.communicationSubMenu ul li').css({
+                    visibility: 'visible',
+                    opacity: '1',
+                    transition: '1s',
+                    top: '2vh',
+                });
+            });
+            $('.donationMenu a').mouseover(function () {
+                $('.donationSubMenu ul li').css({
+                    visibility: 'visible',
+                    opacity: '1',
+                    transition: '1s',
+                    top: '2vh',
+                });
+            });
+        } else {
+            // 화면 크기가 600px 이하인 경우
+            $('.introMenu a').off('mouseover'); // 이벤트 제거
+            $('.activityMenu a').off('mouseover');
+            $('.businessMenu a').off('mouseover');
+            $('.communicationMenu a').off('mouseover');
+            $('.donationMenu a').off('mouseover');
+        }
+    }
+
+    // 초기 상태 설정
+    handleMediaQuery();
+
+    // 미디어 쿼리 리스너 등록
+    mediaQuery.addListener(handleMediaQuery);
+});
+// $(document).ready(function () {
+//     $('.introMenu a').mouseover(function () {
+//         $('.introSubMenu ul li').css({
+//             visibility: 'visible',
+//             opacity: '1',
+//             transition: '1s',
+//             top: '2vh',
+//         });
+//     });
+// });
+// $(document).ready(function () {
+//     $(".activityMenu a").mouseover(function () {
+//         $(".activitySubMenu ul li").css({
+//             visibility: "visible",
+//             opacity: "1",
+//             transition: "1s",
+//             top: "2vh",
+//         });
+//     });
+// });
+// $(document).ready(function () {
+//     $('.businessMenu a').mouseover(function () {
+//         $('.businessSubMenu ul li').css({
+//             visibility: 'visible',
+//             opacity: '1',
+//             transition: '1s',
+//             top: '2vh',
+//         });
+//     });
+// });
+
+// $(document).ready(function () {
+//     $('.communicationMenu a').mouseover(function () {
+//         $('.communicationSubMenu ul li').css({
+//             visibility: 'visible',
+//             opacity: '1',
+//             transition: '1s',
+//             top: '2vh',
+//         });
+//     });
+// });
+
+// $(document).ready(function () {
+//     $('.donationMenu a').mouseover(function () {
+//         $('.donationSubMenu ul li').css({
+//             visibility: 'visible',
+//             opacity: '1',
+//             transition: '1s',
+//             top: '2vh',
+//         });
+//     });
+// });
+
+window.addEventListener('mouseout', function () {
+    $('.main-menu ul li').css({
+        visibility: 'hidden',
+        opacity: '0',
+        transition: '0.5s',
+        top: '0px',
     });
 });
 
 // HEAD
 $(document).ready(function () {
-    var $scrollToTopButton = $("#scrollToTopBtn");
+    var $scrollToTopButton = $('#scrollToTopBtn');
     var scrollThreshold = 0.4; // 40%
 
     // 스크롤이 전체 페이지의 40% 이상 내려갔을 때 버튼 나타나게 구현
@@ -72,27 +139,9 @@ $(document).ready(function () {
         var scrollPosition = $(this).scrollTop();
 
         if (scrollPosition > scrollThreshold * (documentHeight - windowHeight)) {
-            $scrollToTopButton.css("display", "inline-block");
+            $scrollToTopButton.css('display', 'inline-block');
         } else {
-            $scrollToTopButton.css("display", "none");
-        }
-    });
-});
-
-$(document).ready(function () {
-    var $scrollToTopButton = $("#scrollToTopBtn");
-    var scrollThreshold = 0.4; // 40%
-
-    // 스크롤이 전체 페이지의 40% 이상 내려갔을 때 버튼 나타나게 구현
-    $(window).scroll(function () {
-        var windowHeight = $(window).height();
-        var documentHeight = $(document).height();
-        var scrollPosition = $(this).scrollTop();
-
-        if (scrollPosition > scrollThreshold * (documentHeight - windowHeight)) {
-            $scrollToTopButton.css("display", "inline-block");
-        } else {
-            $scrollToTopButton.css("display", "none");
+            $scrollToTopButton.css('display', 'none');
         }
     });
 });
@@ -101,22 +150,9 @@ $(document).ready(function () {
 function scrollToTop() {
     window.scrollTo({
         top: 0,
-        behavior: "smooth",
+        behavior: 'smooth',
     });
 }
-
-// $(document).ready(function () {
-//     $(".goToDonation").click(function () {
-//         location.href = "http://127.0.0.1:5502/html/donation/donation01.html";
-//     });
-// });
-
-// $(document).ready(function () {
-//     $(".cancelButt").click(function () {
-//         $(".donationCard").fadeOut();
-//     });
-// });
-
 
 // 다크모드
 
@@ -132,41 +168,34 @@ document.addEventListener('DOMContentLoaded', function () {
         const bodyElements = body.querySelectorAll('*');
         // bodyElements.forEach(element => element.classList.toggle('dark-mode'));
 
-        bodyElements.forEach(element => {
+        bodyElements.forEach((element) => {
             if (
                 !element.closest('footer') &&
                 !element.closest('div.intro-section') &&
                 // !element.closest('div.main-menu') &&
-                !element.matches('div.main-menu')&&
-                !element.matches('ul.inner')&&
+                !element.matches('div.main-menu') &&
+                !element.matches('ul.inner') &&
                 !element.matches('button#scrollToTopBtn') &&
                 !element.closest('div.donationCard') &&
-
                 // index.html
                 !element.closest('h3.youtubeH3') &&
                 !element.closest('div.goToNoticeSection') &&
                 !element.closest('div.goToBussinessSection') &&
                 !element.closest('div.goToCampaign *') &&
                 // activation
-                !element.closest('div.join-us-section') 
-
-
+                !element.closest('div.join-us-section')
             ) {
-
                 element.classList.toggle('dark-mode');
-                
             }
         });
 
         mode = !mode;
-        if(mode){
+        if (mode) {
             this.textContent = '라이트 모드';
-            darkModeToggle.setAttribute('style','background-color: #FFF !important; color:black !important')
-        }
-        else{
+            darkModeToggle.setAttribute('style', 'background-color: #FFF !important; color:black !important');
+        } else {
             this.textContent = '다크 모드';
-            darkModeToggle.setAttribute('style','background-color: #333; color:white')
+            darkModeToggle.setAttribute('style', 'background-color: #333; color:white');
         }
-    
     });
 });
